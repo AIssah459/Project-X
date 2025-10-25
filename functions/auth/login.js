ba38762c9316bb607c221d29315f47ff4297197f
AIssah459 <96153984+AIssah459@users.noreply.github.com>
2025-10-24 21:58:29 -0400

    Add files via upload

--- functions/auth/login.js
+++ functions/auth/login.js
@@ -1,35 +1,12 @@
-export async function onRequestPost(context) {
+export async function onRequestPost({request, env}) {
+    const backendUrl = `${env.RAILWAY_API_URL}/auth/login`;
 
-    const req = context.req;
-
-    console.log(req);
-
-    return new Response("Hello from Cloudflare Pages Function!", {
-    headers: {
-      "content-type": "text/plain"
-    },
-    status: 200 // OK
-  });
-    // const body = await req.json();
-
-    // const backendUrl = process.env.RAILWAY_BACKEND_URL;
-
-    // // Forward request to your Railway backend
-    // const res = await fetch(`${backendUrl}/auth/login`, {
-    //     method: 'POST',
-    //     headers: {'Content-Type': 'application/json'},
-    //     body: JSON.stringify(body),
-    //     credentials: 'include'
-    // });
-
-    // const cookie = res.headers.get('set-cookie');
-    // const data = await res.json();
-
-    // return new Response(JSON.stringify(data), {
-    //     status: res.status,
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Set-Cookie': cookie || ''
-    //     }
-    // });
+    const backendResponse = await fetch(backendUrl, {
+        method: "POST",
+        credentials: "include",
+        headers: request.headers,
+        body: request.body,
+        redirect: "manual"
+    });
+    return new Response(backendResponse.body, backendResponse);
 }
